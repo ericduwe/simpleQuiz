32509c0ae2f32dd0572a8ccfcbde2a870915e53e
Eric Duwe <ericduwe@gmail.com>
2021-08-09 17:05:16 -0500

    added questions to new array of question objects

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
 
 
 
-let question = {
+let question = [
+    {
     state: "Texas",
     answers: ["Houston", "Dallas", "San Antonio", "Austin"],
     correctAnswer: 3
-};
+    },
+    {
+    state: "New York",
+    answers: ["Syracuse", "Albany", "New York City", "Niagara Falls"],
+    correctAnswer: 1
+    },
+    {
+    state: "California",
+    answers: ["Oakland", "Sacramento", "Los Angeles", "San Jose"],
+    correctAnswer: 1
+    }
 
+]
 function askQuestion (challenge) {
     let stateEl = document.querySelector("#state-name");
     stateEl.textContent = challenge.state;
